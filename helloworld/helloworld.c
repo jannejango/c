a738f889c185572bc3e348c97185f448713f9cd2
jannejango <jannejango@users.noreply.github.com>
2014-11-17 23:16:32 +0900

    helloworld and its preprocess output and its assemble.

--- helloworld/helloworld.c
+++ helloworld/helloworld.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
+
 void main(){
-	print("hello world2\n");
-}
-void print(char *str){
-	printf("%s\n", str);
+  printf("hello world!");
 }
---
+++ helloworld/helloworld.s
@@ -0,0 +1,27 @@
+	.file	"helloworld.c"
+	.section	.rodata
+.LC0:
+	.string	"hello world!"
+	.text
+.globl main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$.LC0, %eax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (GNU) 4.4.7 20120313 (Red Hat 4.4.7-4)"
+	.section	.note.GNU-stack,"",@progbits
